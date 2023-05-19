0664519ccf44aa063847fbd8b221f7d9fe6944f6
rama1997 <maraymond@yahoo.com>
2023-05-18 17:55:11 -0700

    Added some new brands

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BRAND_NAMES = ["santoni", r"brunello%20cucinelli", "off-white™", "gucci", "prada"]
+BRAND_NAMES = ["santoni", r"brunello%20cucinelli", "off-white™", "gucci", "prada", r"polo%20ralph%20lauren", r"saint%20laurent", "versace", "balenciaga", r"pantofola%20d'oro"]
 
-BRAND_NUMBERS = ["615", "942", "32300", "42", "63"]
+BRAND_NUMBERS = ["615", "942", "32300", "42", "63", "165", "2449", "1889", "221", "2581"]
 
 DESIRED_DISCOUNT = 80
