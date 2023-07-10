185ad0c00e2c8e4e3d6544fae49e8084e964bc56
rama1997 <maraymond@yahoo.com>
2023-07-10 14:48:53 -0700

    Update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-BRAND_NAMES = ["santoni", r"brunello%20cucinelli", "off-white™", "gucci", "prada", r"polo%20ralph%20lauren", r"saint%20laurent", "versace", "balenciaga", r"pantofola%20d'oro"]
+BRAND_NAMES = ["santoni", r"brunello%20cucinelli", "off-white™", "gucci", "prada", r"polo%20ralph%20lauren", r"saint%20laurent", "versace", "balenciaga"]
 
-BRAND_NUMBERS = ["615", "942", "32300", "42", "63", "165", "2449", "1889", "221", "2581"]
+BRAND_NUMBERS = ["615", "942", "32300", "42", "63", "165", "2449", "1889", "221"]
 
 # Sale Percentage Off. 80 means the script will search for items that are 80% off
 DESIRED_DISCOUNT = 80
